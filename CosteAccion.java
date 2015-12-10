0972ab1b9cfad8b079f4ddc734fc49789c063709
nopesal <nssalomone@gmail.com>
2015-12-10 22:10:33 +0100

    CosteAccion cambiado a un switch
    También se ha comentado.

--- CosteAccion.java
+++ CosteAccion.java
@@ -9,19 +9,36 @@ import aima.core.search.framework.StepCostFunction;
 public class CosteAccion implements StepCostFunction {
 	@Override
 	public double c(Object s, Action a, Object sDelta) {
-		Estado estado = (Estado) s; // Estado sobre el que se aplica la accion
-		
+		Estado estado = (Estado) s; // Estado sobre el que se aplica la acción
+
 		double cost = 1.0;
-		
-		if (Ejecutar.getObstaculo(estado.x, estado.y) == 'W') {
+
+		/**
+		 * El coste de moverse por el mapa corresponde al de traspasar el
+		 * obstáculo de la casilla actual. Es por eso que no son necesarios ni
+		 * la accion a ejecutarse ni el estado sucesor.
+		 */
+		switch (Ejecutar.getObstaculo(estado.x, estado.y)) {
+		/* El coste de traspasar el agua es 2 */
+		case 'W':
 			cost = 2.0;
-		}else if(Ejecutar.getObstaculo(estado.x, estado.y) == 'S'){
+			break;
+		/* El coste de traspasar arena es 2 */
+		case 'S':
 			cost = 2.0;
-		}else if(Ejecutar.getObstaculo(estado.x, estado.y) == 'T'){
+			break;
+		/* El coste de traspasar un bosque es 4 */
+		case 'T':
 			cost = 4.0;
-		}else if(Ejecutar.getObstaculo(estado.x, estado.y) == '.'){
+			break;
+		/* El coste no traspasar ningún obstáculo es 1 */
+		case '.':
 			cost = 1.0;
+			break;
+		default:
+			break;
 		}
+
 		return cost;
 
 	}
